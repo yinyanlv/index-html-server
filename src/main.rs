4ce5583eb8e7ecc83c98663f37cac00d6ff18b59
yinyanlv <yanlv.yin@gmail.com>
2019-07-19 17:54:11 +0800

    add: add lazy_static

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate actix_files;
 #[macro_use]
 extern crate serde_json;
 extern crate clap;
+#[macro_use]
+extern crate lazy_static;
 
 mod config;
 
@@ -15,65 +17,78 @@ use serde_json::Value;
 use clap::{App as ClapApp, Arg, ArgMatches};
 use config::JsonConfig;
 
-fn main() -> std::io::Result<()> {
+lazy_static! {
 
-    let clap_matches = init_clap();
+    static ref CLAP_MATCHES: ArgMatches<'static> = {
+        init_clap()
+    };
 
-    let config_path = clap_matches.value_of("config");
+    static ref CONFIG: Value = {
+        let config_path = CLAP_MATCHES.value_of("config");
 
-    // 配置优先级：自定义配置文件 > 当前文件夹config.json > 默认配置
-    let config: Value = match config_path {
-        Some(path) => JsonConfig::new(path).unwrap().get_value(),
-        None => {
-            let default_path = "./config.json";
-            
-            if Path::new(default_path).exists() {
-                JsonConfig::new(default_path).unwrap().get_value()
-            } else {
-                json!({
-                    "folderPath": "./static/",
-                    "entryFile": "index.html", 
-                    "port": 8000
-                })
+        // 配置优先级：自定义配置文件 > 当前文件夹config.json > 默认配置
+        match config_path {
+            Some(path) => JsonConfig::new(path).unwrap().get_value(),
+            None => {
+                let default_path = "./config.json";
+                
+                if Path::new(default_path).exists() {
+                    JsonConfig::new(default_path).unwrap().get_value()
+                } else {
+                    json!({
+                        "folderPath": "./static/",
+                        "entryFile": "index.html", 
+                        "port": 8000
+                    })
+                }
             }
         }
     };
 
-    let _folder_path = config.get("folderPath")
-                        .map(|val| {
-                            if let Some(text) = val.as_str() {
-                                text.to_string()
-                            } else {
-                                "./static/".to_string()
-                            }
-                        })
-                        .unwrap_or("./static/".to_string());
+    static ref FOLDER_PATH: String = {
+        CONFIG.get("folderPath")
+            .map(|val| {
+                if let Some(text) = val.as_str() {
+                    text.to_string()
+                } else {
+                    "./static/".to_string()
+                }
+            })
+            .unwrap_or("./static/".to_string())
+    };
 
-    let _entry_file = config.get("entryFile")
-                        .map(|val| {
-                            if let Some(text) = val.as_str() {
-                                text.to_string()
-                            } else {
-                                "index.html".to_string()
-                            }
-                        })
-                        .unwrap_or("index.html".to_string());
+    static ref ENTRY_FILE: String = {
+        CONFIG.get("entryFile")
+            .map(|val| {
+                if let Some(text) = val.as_str() {
+                    text.to_string()
+                } else {
+                    "index.html".to_string()
+                }
+            })
+            .unwrap_or("index.html".to_string())
+    };
 
-    let port = config.get("port")
-                        .map(|val| {
-                            if let Some(number) = val.as_u64() {
-                                number
-                            } else {
-                                3000
-                            }
-                        })
-                        .unwrap_or(3000);
+    static ref PORT: u64 = {
+        CONFIG.get("port")
+            .map(|val| {
+                if let Some(number) = val.as_u64() {
+                    number
+                } else {
+                    3000
+                }
+            })
+            .unwrap_or(3000)
+    };
+}
 
-    let bind_str = &*format!("127.0.0.1:{}", port);
+fn main() -> std::io::Result<()> {
 
-    println!("Server for index.html is listening on port {}！", port);
+    let bind_str = &*format!("127.0.0.1:{}", PORT.to_string());
 
-    HttpServer::new(move || {
+    println!("Server for index.html is listening on port {}！", PORT.to_string());
+
+    HttpServer::new(|| {
        App::new()
         .route("/{file_path:.*}", web::get().to(handle))
     })
@@ -83,19 +98,16 @@ fn main() -> std::io::Result<()> {
 
 fn handle(req: HttpRequest) -> Result<NamedFile, Error> {
 
-    let folder_path = "./static/";
-    let entry_file = "index.html";
-
     let mut file_path: &str = req.match_info().get("file_path").unwrap_or("");
 
     if file_path == "" {
-        file_path = entry_file.clone();
+        file_path = &*ENTRY_FILE;
     }
 
-    let mut real_path = folder_path.to_string() + file_path;
+    let mut real_path = FOLDER_PATH.to_string() + file_path;
     let temp_path = Path::new(&*real_path);
     if !temp_path.exists() || temp_path.is_dir() {
-        real_path = folder_path.to_string() + entry_file.clone();
+        real_path = FOLDER_PATH.to_string() + &*ENTRY_FILE;
     }
 
     let path_buf: PathBuf = real_path.parse().unwrap();
